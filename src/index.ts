3410f67ce1da4975dbbe87e61f6179add915855d
unique boy <information11993@gmail.com>
2023-01-25 18:44:42 +0545

    all done

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+class Log{
+    static success(msg : string){
+        console.log(`%c ${msg}`, "color:green")
+    }
+    static danger(msg : string){
+        console.log(`%c ${msg}`, "color:red")
+    }
+    static info(msg : string){
+        console.log(`%c ${msg}`, "color:black;background:yellow")
+    }
+}
+
+export default Log;
